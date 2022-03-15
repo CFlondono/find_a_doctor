67f913af5b785f1b21632bb9d3caf6276bd7be2e
Claudia Londono <claudia@londonos.com>
2022-03-15 16:17:25 -0500

    changed conexion string

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/holidaysDB'
+const MONGO_URI = process.env.DB_URI || 'mongodb+srv://clondono:Luna2021@cluster0.hjbuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const MONGO_URI = 'mongodb://127.0.0.1:27017/doctorsDB'
+
 
 mongoose.connect(MONGO_URI, ()=>{
     console.log('connected to MongoDB - DoctorsDB')
